eb1c12ce8f353261096bfdeeafa1e47c68a53d24
Pauwiee05 <127363410+Pauwiee05@users.noreply.github.com>
2023-09-07 09:50:45 +0800

    Update FA3.R

--- FA3.R
+++ FA3.R
@@ -6,28 +6,19 @@ examscores <- c(88, 45, 53, 86, 33, 86, 85, 30, 89, 53, 41, 96, 56, 38, 62,
 
 # Calculate Q1, Q2, Q3, D9, P95
 
-#FOR QUARTILES
+#COMPUTATIONS
 quartile_1 <- quantile(examscores, 0.25)
-# the value Q2 being equal to the median
 quartile_2 <- median(examscores)
 quartile_3 <- quantile(examscores, 0.75)
+decile_9 <- quantile(examscores, 0.9)
+percentile_95 <- quantile(examscores, 0.95)
 
-#QUARTILE RESULTS
+# #RESULTS
 cat("1st Quartile:", quartile_1, "\n")
 cat("2nd Quartile:", quartile_2, "\n")
 cat("3rd Quartile:", quartile_3, "\n")
-
-
-#FOR DECILE
-decile_9 <- quantile(examscores, 0.9)
-
-#DECILE RESULTS
 cat("9th Decile:", decile_9, "\n")
-
-
-#FOR PERCENTILE
-percentile_95 <- quantile(examscores, 0.95)
-
-#PERCENNTILE RESULTS
 cat("95th Percentile:", percentile_95, "\n")
+
+
 
